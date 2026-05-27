1f967151e2eca57489bcd261fa6c219fe46eccaf
Maximilian Fischer <maximilian.fischer@crg.eu>
2026-05-27 16:17:23 +0200

    Increase default BAM_RECORDS limit to 1000000 for improved processing capacity.

--- ingestionQC_stdin_debug.sh
+++ ingestionQC_stdin_debug.sh
@@ -557,7 +557,7 @@ check_vcf_stdin() {
 # BAM
 ##############################################################################
 
-BAM_RECORDS="${BAM_RECORDS:-100000}"
+BAM_RECORDS="${BAM_RECORDS:-1000000}"
 
 bam_samtools_check() {
     debug_log "bam_samtools_check: started"
